9d56322224008a2a4fa17579c991f5488df9adfd
srejas <srejas@outlook.com>
2024-01-15 11:09:39 -0700

    Week 02 Learning Activity Completed

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,13 @@
+using System.Configuration.Assemblies;
+
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+    public void DisplayJobInfo()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
