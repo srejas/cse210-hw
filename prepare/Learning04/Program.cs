fb99aa5acde00afa9827c78f2ac178765fe4a16a
srejas <srejas@outlook.com>
2024-01-30 22:19:52 -0700

    Learning 04 Completed

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment testAssingment1 = new MathAssignment("Tom Smith", "Fractions", "Section 7.9", "Problems 8-19");
+        Console.WriteLine(testAssingment1.GetSummary());
+        Console.WriteLine(testAssingment1.GetHomeworkList());
+        
+        WritingAssignment testAssignment2 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(testAssignment2.GetSummary());
+        Console.WriteLine(testAssignment2.GetWritingAssingment());
     }
 }
