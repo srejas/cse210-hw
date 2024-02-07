d5fe103c823c335ae657c71b0d174d197f5d7b49
srejas <srejas@outlook.com>
2024-02-06 20:24:07 -0700

    Learning 05 Completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square newSquare = new Square("black", 20);
+
+        Rectangle newRectangle = new Rectangle("White", 10, 5);
+
+        Circle newCircle = new Circle("Red", 15);
+
+        List<Shape> testList = new List<Shape>();
+
+        testList.Add(newCircle);
+        testList.Add(newRectangle);
+        testList.Add(newSquare);
+
+        Console.WriteLine();
+        
+        foreach (Shape shape in testList)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
